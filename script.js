5f77681a616400718305f2b70cd43134983c77b6
lorenzoworx <asocialinfroverf@gmail.com>
2022-08-02 19:48:54 +0100

    Linter errors

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector('.hamburger');
 
-hamburger.addEventListener("click", function() {
-    let menu = document.querySelector(".hamburger-menu");
-    let top = document.querySelector(".top");
-    let middle = document.querySelector(".middle");
-    let bottom = document.querySelector(".bottom");
-    menu.classList.toggle("hide");
-    top.classList.toggle("active");
-    middle.classList.toggle("active");
-    bottom.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  const menu = document.querySelector('.hamburger-menu');
+  const top = document.querySelector('.top');
+  const middle = document.querySelector('.middle');
+  const bottom = document.querySelector('.bottom');
+  menu.classList.toggle('hide');
+  top.classList.toggle('active');
+  middle.classList.toggle('active');
+  bottom.classList.toggle('active');
+});
 
-let items =document.querySelector(".hamburger-menu").childNodes;
+const items = document.querySelector('.hamburger-menu').childNodes;
 
-items.forEach(item => {
-    item.addEventListener("click", function() {
-        let menu = document.querySelector(".hamburger-menu");
-        let top = document.querySelector(".top");
-        let middle = document.querySelector(".middle");
-        let bottom = document.querySelector(".bottom");
-        menu.classList.toggle("hide");
-        top.classList.toggle("active");
-        middle.classList.toggle("active");
-        bottom.classList.toggle("active");
-    })
-})
+items.forEach((item) => {
+  item.addEventListener('click', () => {
+    const menu = document.querySelector('.hamburger-menu');
+    const top = document.querySelector('.top');
+    const middle = document.querySelector('.middle');
+    const bottom = document.querySelector('.bottom');
+    menu.classList.toggle('hide');
+    top.classList.toggle('active');
+    middle.classList.toggle('active');
+    bottom.classList.toggle('active');
+  });
+});
